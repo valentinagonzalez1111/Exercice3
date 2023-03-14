cc7ff26be2d9e01b4ba88c3a8392f4f367d6c9e3
Valentina <balendi.art@gmail.com>
2023-03-14 11:42:16 +0100

    Initial Commit

---
+++ Exercice3/Exercice3App.swift
@@ -0,0 +1,17 @@
+//
+//  Exercice3App.swift
+//  Exercice3
+//
+//  Created by Apprenant 18 on 14/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct Exercice3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
